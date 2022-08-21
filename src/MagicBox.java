d7e9a7d30bbca270abaf247710b0641898b44db5
Svetlana Tokarchuk <lanawm73@gmail.com>
2022-08-21 18:14:40 -0400

    Revision complited

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,19 +1,19 @@
 import java.util.Random;
-public class MagicBox <T> {
+
+public class MagicBox<T> {
     protected T[] items;
+    protected int filled = 0;
+    Random random = new Random();
+
     public MagicBox(int maxItems) {
         this.items = (T[]) new Object[maxItems];
     }
-
-    int filled = 0;
-    Random random = new Random();
-    int randomInt = random.nextInt(5);
 
     public boolean add(T item) {
         for (int i = 0; i < items.length; i++) {
             if (items[i] == null) {
                 items[i] = item;
-                filled+=1;
+                filled += 1;
                 return true;
             }
         }
@@ -21,7 +21,7 @@ public class MagicBox <T> {
     }
 
     public T pick() throws RuntimeException {
-        for (int i = 0; i < items.length; i++){
+        for (int i = 0; i < items.length; i++) {
             if (items[i] == null) {
                 throw new RuntimeException("Коробка не полна, заполните " + (items.length - filled) + " ячеек");
             } else {
